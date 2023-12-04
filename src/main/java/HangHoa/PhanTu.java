c458a2dbe6facad2cee1471ef066f490ea44a34f
Remilia172112 <trangianguyen.com@gmail.com>
2023-12-04 10:02:42 +0700

    Update phần tử

--- src/main/java/HangHoa/PhanTu.java
+++ src/main/java/HangHoa/PhanTu.java
@@ -4,14 +4,35 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public abstract class PhanTu {
+    public static Scanner sc  =new Scanner(System.in);
     // Tạo tháng năm hiện tại
     LocalDate localDate = LocalDate.now();
     int namhientai = localDate.getYear();
     int thanghientai = localDate.getMonthValue();
     int ngayhientai = localDate.getDayOfMonth();
 
-    public static Scanner sc  =new Scanner(System.in);
+    String ma;
+    String ten;
+    int sl;
     
+    public String getMa() {
+        return ma;
+    }
+    public void setMa(String ma) {
+        this.ma = ma;
+    }
+    public String getTen() {
+        return ten;
+    }
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    public int getSl() {
+        return sl;
+    }
+    public void setSl(int sl) {
+        this.sl = sl;
+    }
     public void nhap() {
         System.out.printf("Noi dung xuat");
     }
